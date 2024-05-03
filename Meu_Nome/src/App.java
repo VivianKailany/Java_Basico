f9893468a3fabb787372e525058cfa3788a84967
VivianKailany <viviankailanymarques@gmail.com>
2024-05-03 19:17:03 -0300

    nova atualização

---
+++ Meu_Nome/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String primeiroNome = "vivian";
+        String segundoNome = "kailany";
+        String nomeCompleto = nomeCompleto ( primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+    public static String nomeCompleto(String primeiroNome,String segundoNome){
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
